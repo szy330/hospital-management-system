ba0a2ba1eda658fad4aab4862394e1c1118768d6
SeasOn <869349578@qq.com>
2020-10-30 10:48:35 +0800

    上传排班和药房接口

---
+++ hms-scheduling-service/src/main/java/com/hms/service/impl/PaibanServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hms.service.impl;
+
+import com.hms.pojo.Paiban;
+import com.hms.mapper.PaibanMapper;
+import com.hms.service.PaibanService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author szy
+ * @since 2020-10-24
+ */
+@Service
+public class PaibanServiceImpl extends ServiceImpl<PaibanMapper, Paiban> implements PaibanService {
+
+}
